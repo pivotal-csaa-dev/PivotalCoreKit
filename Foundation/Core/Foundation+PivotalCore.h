2275d649d0a5934d3a1afb9e06a534af616a6cee
Kai Lee <hlee@pivotal.io>
2015-05-21 14:02:41 -0700

    Remove PCKXMLParser from Foundation/Core

--- Foundation/Core/Foundation+PivotalCore.h
+++ Foundation/Core/Foundation+PivotalCore.h
@@ -1,5 +1,4 @@
 #import "NSString+PivotalCore.h"
-#import "NSData+PivotalCore.h"
 #import "NSArray+PivotalCore.h"
 #import "NSObject+MethodDecoration.h"
 #import "NSObject+MethodRedirection.h"
@@ -8,9 +7,6 @@
 #import "NSURL+QueryComponents.h"
 
 #import "NSURLConnectionDelegate.h" // For pre-10.7 and pre-iOS5
-#import "PCKHTTPInterface.h"
 
 #import "PCKParser.h"
 #import "PCKResponseParser.h"
-#import "PCKXMLParser.h"
-#import "PCKXMLParserDelegate.h"
